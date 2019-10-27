52eaef73aeaba378539f66ac630ac44af30e3aa6
satz8782 <57072835+satz8782@users.noreply.github.com>
2019-10-28 00:11:39 +0530

    Update rules.php

--- rules.php
+++ rules.php
@@ -30,7 +30,7 @@ require 'dbConnect.php';
     <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queriesasnsad -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
